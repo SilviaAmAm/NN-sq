6de44db1ca58ff9a3b24d3cdc900a5497596f397
SilviaAmAm <silvia.amabilino@bristol.ac.uk>
2019-01-31 15:13:05 +0000

    Started modifying the files to do hyperparameter optimisation

--- scaler/scaled_energies.py
+++ scaler/scaled_energies.py
@@ -8,15 +8,15 @@ data_methane = h5py.File("/Volumes/Transcend/repositories/NN-sq/data_sets/methan
 data_isopentane = h5py.File("/Volumes/Transcend/repositories/NN-sq/data_sets/isopentane_cn_dft.hdf5", "r")
 data_2isohex = h5py.File("/Volumes/Transcend/repositories/NN-sq/data_sets/2isohexane_cn_dft_pruned.hdf5", "r")
 data_3isohex = h5py.File("/Volumes/Transcend/repositories/NN-sq/data_sets/3isohexane_cn_dft_pruned.hdf5", "r")
-data_dimer = h5py.File("/Volumes/Transcend/repositories/NN-sq/data_sets/isopentane_dimer_cn_dft.hdf5", "r")
+data_squal = h5py.File("/Volumes/Transcend/repositories/NN-sq/data_sets/squalane_cn_dft.hdf5", "r")
 
 ref_ene = -133.1 * 2625.50
 
 n_samples = 5000
 
-ene_dimer = np.array(data_dimer.get("ene")[:n_samples]) * 2625.50
-ene_dimer = ene_dimer - ref_ene
-zs_dimer = np.array(data_dimer.get("zs")[:n_samples], dtype=np.int32)
+ene_squal = np.array(data_squal.get("ene")[:n_samples]) * 2625.50
+ene_squal = ene_squal - ref_ene
+zs_squal = np.array(data_squal.get("zs")[:n_samples], dtype=np.int32)
 
 ene_3hex = np.array(data_3isohex.get("ene")[:n_samples]) * 2625.50
 ene_3hex = ene_3hex - ref_ene
@@ -34,8 +34,8 @@ ene_methane = np.array(data_methane.get("ene")[:n_samples]) * 2625.50
 ene_methane = ene_methane - ref_ene
 zs_methane = np.array(data_methane.get("zs")[:n_samples], dtype=np.int32)
 
-zs_for_scaler_long = list(zs_methane) + list(zs_isopent) + list(zs_2hex) + list(zs_3hex) + list(zs_dimer)
-concat_ene = np.concatenate((ene_methane, ene_isopent, ene_2hex, ene_3hex, ene_dimer))
+zs_for_scaler_long = list(zs_methane) + list(zs_isopent) + list(zs_2hex) + list(zs_3hex) + list(zs_squal)
+concat_ene = np.concatenate((ene_methane, ene_isopent, ene_2hex, ene_3hex, ene_squal))
 
 scaling = pickle.load(open("./scaler.pickle", "rb"))
 concat_ene_scaled = scaling.transform(zs_for_scaler_long, concat_ene)
