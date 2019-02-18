5f4a5c1b4d4131e37b79c190c93bc50478eaf878
SilviaAmAm <silvia.amabilino@bristol.ac.uk>
2019-02-18 11:12:01 +0000

    Modifications for learning curve and retraining

---
+++ training/methane_isopent_isohex/model2/train.py
@@ -0,0 +1,124 @@
+from qml.aglaia.aglaia import ARMP
+import numpy as np
+import h5py
+import pickle
+from random import shuffle
+
+
+# Getting the dataset
+data_methane = h5py.File("../../../data_sets/methane_cn_dft.hdf5", "r")
+data_isopentane = h5py.File("../../../data_sets/isopentane_cn_dft.hdf5", "r")
+data_squal = h5py.File("../../../data_sets/squalane_cn_dft.hdf5", "r")
+data_2isohex = h5py.File("../../../data_sets/2isohexane_cn_dft_pruned.hdf5")
+data_3isohex = h5py.File("../../../data_sets/3isohexane_cn_dft_pruned.hdf5")
+
+ref_ene = -133.1 * 2625.50
+
+n_samples_methane = 6000
+n_samples_isopentane = 4000
+n_samples_2isohex = 2500
+n_samples_3isohex = 2500
+
+zs_squal = np.array(data_squal.get("zs"), dtype=np.int32)
+n_atoms_squal = len(zs_squal[0])
+print("The number of squalane samples is: %i" % len(zs_squal))
+
+# Data for methane
+traj_idx_methane = np.asarray(data_methane.get('traj_idx'), dtype=int)
+
+idx_train_methane = np.where(traj_idx_methane != 14)[0]
+shuffle(idx_train_methane)
+idx_train_methane = idx_train_methane[:n_samples_methane]
+
+print("The number of methane samples is: %i (train)" % (len(idx_train_methane)))
+
+xyz_methane = np.array(data_methane.get("xyz"))[idx_train_methane]
+zs_methane = np.array(data_methane.get("zs"), dtype=np.int32)[idx_train_methane]
+ene_methane = np.array(data_methane.get("ene"))[idx_train_methane]* 2625.50 - ref_ene
+
+pad_xyz_methane = np.concatenate((xyz_methane, np.zeros((xyz_methane.shape[0], n_atoms_squal - xyz_methane.shape[1], 3))), axis=1)
+pad_zs_methane = np.concatenate((zs_methane, np.zeros((zs_methane.shape[0], n_atoms_squal - zs_methane.shape[1]), dtype=np.int32)), axis=1)
+
+# Data for isopentane
+traj_idx_isopentane = np.asarray(data_isopentane.get('traj_idx'), dtype=int)
+
+idx_train_isopentane = np.where(traj_idx_isopentane != 22)[0]
+shuffle(idx_train_isopentane)
+idx_train_isopentane = idx_train_isopentane[:n_samples_isopentane]
+
+print("The number of isopentane samples is: %i (train)" % (len(idx_train_isopentane)))
+
+xyz_isopentane = np.array(data_isopentane.get("xyz"))[idx_train_isopentane]
+zs_isopentane = np.array(data_isopentane.get("zs"), dtype=np.int32)[idx_train_isopentane]
+ene_isopentane = np.array(data_isopentane.get("ene"))[idx_train_isopentane]* 2625.50 - ref_ene
+
+pad_xyz_isopentane = np.concatenate((xyz_isopentane, np.zeros((xyz_isopentane.shape[0], n_atoms_squal - xyz_isopentane.shape[1], 3))), axis=1)
+pad_zs_isopentane = np.concatenate((zs_isopentane, np.zeros((zs_isopentane.shape[0], n_atoms_squal - zs_isopentane.shape[1]), dtype=np.int32)), axis=1)
+
+# Data for 2-isohexane
+traj_idx_2isohex = np.asarray(data_2isohex.get('traj_idx'), dtype=int)
+
+idx_train_2isohex = np.where(traj_idx_2isohex != 12)[0]
+shuffle(idx_train_2isohex)
+idx_train_2isohex = idx_train_2isohex[:n_samples_2isohex]
+
+print("The number of 2-isohexane samples is: %i (train)" % (len(idx_train_2isohex)))
+
+xyz_2isohex = np.array(data_2isohex.get("xyz"))[idx_train_2isohex]
+zs_2isohex = np.array(data_2isohex.get("zs"), dtype=np.int32)[idx_train_2isohex]
+ene_2isohex = np.array(data_2isohex.get("ene"))[idx_train_2isohex]* 2625.50 - ref_ene
+
+pad_xyz_2isohex = np.concatenate((xyz_2isohex, np.zeros((xyz_2isohex.shape[0], n_atoms_squal - xyz_2isohex.shape[1], 3))), axis=1)
+pad_zs_2isohex = np.concatenate((zs_2isohex, np.zeros((zs_2isohex.shape[0], n_atoms_squal - zs_2isohex.shape[1]), dtype=np.int32)), axis=1)
+
+# Data for 3-isohexane
+traj_idx_3isohex = np.asarray(data_3isohex.get('traj_idx'), dtype=int)
+
+idx_train_3isohex = np.where(traj_idx_3isohex != 14)[0]
+shuffle(idx_train_3isohex)
+idx_train_3isohex = idx_train_3isohex[:n_samples_3isohex]
+
+print("The number of 3-isohexane samples is: %i (train)" % (len(idx_train_3isohex)))
+
+xyz_3isohex = np.array(data_3isohex.get("xyz"))[idx_train_3isohex]
+zs_3isohex = np.array(data_3isohex.get("zs"), dtype=np.int32)[idx_train_3isohex]
+ene_3isohex = np.array(data_3isohex.get("ene"))[idx_train_3isohex]* 2625.50 - ref_ene
+
+pad_xyz_3isohex = np.concatenate((xyz_3isohex, np.zeros((xyz_3isohex.shape[0], n_atoms_squal - xyz_3isohex.shape[1], 3))), axis=1)
+pad_zs_3isohex = np.concatenate((zs_3isohex, np.zeros((zs_3isohex.shape[0], n_atoms_squal - zs_3isohex.shape[1]), dtype=np.int32)), axis=1)
+
+
+# Concatenating all the data
+concat_xyz = np.concatenate((pad_xyz_methane, pad_xyz_isopentane, pad_xyz_2isohex, pad_xyz_3isohex))
+concat_ene = np.concatenate((ene_methane, ene_isopentane, ene_2isohex, ene_3isohex))
+concat_zs = np.concatenate((pad_zs_methane, pad_zs_isopentane, pad_zs_2isohex, pad_zs_3isohex))
+
+zs_for_scaler = list(zs_methane) + list(zs_isopentane) + list(zs_2isohex) + list(zs_3isohex)
+
+scaling = pickle.load(open("../../../scaler/scaler.pickle", "rb"))
+concat_ene_scaled = scaling.transform(zs_for_scaler, concat_ene)
+
+# ACSF parameters
+n_basis = 14
+r_min = 0.8
+r_cut = 3.248470148281216
+tau = 1.6110162523935854
+eta = 4 * np.log(tau) * ((n_basis-1)/(r_cut - r_min))**2
+zeta = - np.log(tau) / (2*np.log(np.cos(np.pi/(4*n_basis-4))))
+
+acsf_params={"nRs2":n_basis, "nRs3":n_basis, "nTs":n_basis, "rcut":r_cut, "acut":r_cut, "zeta":zeta, "eta":eta}
+
+# Generate estimator
+estimator = ARMP(iterations=822, l1_reg=1.7465959510257397e-05, l2_reg=6.690500063362741e-06, learning_rate=0.0008983333675453145, representation_name='acsf',
+                 representation_params=acsf_params, tensorboard=True, store_frequency=10, hidden_layer_sizes=(70,55,), batch_size=69)
+
+estimator.set_properties(concat_ene_scaled)
+estimator.generate_representation(concat_xyz, concat_zs, method='fortran')
+
+# Training and testing
+idx_training = list(range(len(concat_ene_scaled)))
+shuffle(idx_training)
+
+estimator.fit(idx_training)
+
+estimator.save_nn()
---
+++ training/methane_isopentane_1/predict.py
@@ -0,0 +1,122 @@
+from qml.qmlearn.preprocessing import AtomScaler
+from qml.aglaia.aglaia import ARMP
+import numpy as np
+import h5py
+import matplotlib.pyplot as plt
+from sklearn import model_selection as modsel
+import pickle
+from random import shuffle
+
+def sort(traj_idx, fn, ene, xyz, zs):
+    # Sorting the trajectories
+    idx_sorted = traj_idx.argsort()
+
+    ene = ene[idx_sorted]
+    xyz = xyz[idx_sorted]
+    zs = zs[idx_sorted]
+    traj_idx = traj_idx[idx_sorted]
+    fn = fn[idx_sorted]
+
+    n_traj = np.unique(traj_idx)
+
+    for item in n_traj:
+        indices = np.where(traj_idx == item)
+
+        idx_sorted = fn[indices].argsort()
+
+        ene[indices] = ene[indices][idx_sorted]
+        traj_idx[indices] = traj_idx[indices][idx_sorted]
+        fn[indices] = fn[indices][idx_sorted]
+        xyz[indices] = xyz[indices][idx_sorted]
+        zs[indices] = zs[indices][idx_sorted]
+        
+    return traj_idx, fn, ene, xyz, zs
+    
+
+# Getting the dataset
+data_methane = h5py.File("../../data_sets/methane_cn_dft.hdf5", "r")
+data_isopentane = h5py.File("../../data_sets/isopentane_cn_dft.hdf5", "r")
+data_squal = h5py.File("../../data_sets/squalane_cn_dft.hdf5", "r")
+
+ref_ene = -133.1 * 2625.50
+
+# Squalane data
+xyz_squal = np.array(data_squal.get("xyz"))
+ene_squal = np.array(data_squal.get("ene")) * 2625.50
+ene_squal = ene_squal - ref_ene
+zs_squal = np.array(data_squal.get("zs"), dtype=np.int32)
+n_atoms_squal = len(zs_squal[0])
+idx_squal = np.asarray(data_squal.get('traj_idx'), dtype=int)
+fn_squal = np.asarray(data_squal.get('Filenumber'), dtype=int)
+
+idx_squal, fn_squal, ene_squal, xyz_squal, zs_squal = sort(idx_squal, fn_squal, ene_squal, xyz_squal, zs_squal)
+
+# Isopentane
+idx_isopentane = np.asarray(data_isopentane.get('traj_idx'), dtype=int)
+idx_isopentane_traj = np.where(idx_isopentane == 22)[0]
+
+xyz_isopentane = np.array(data_isopentane.get("xyz"))[idx_isopentane_traj]
+zs_isopentane = np.array(data_isopentane.get("zs"), dtype=np.int32)[idx_isopentane_traj]
+ene_isopentane = np.array(data_isopentane.get("ene"))[idx_isopentane_traj]*2625.5 - ref_ene
+
+pad_xyz_isopentane = np.concatenate((xyz_isopentane, np.zeros((xyz_isopentane.shape[0], n_atoms_squal - xyz_isopentane.shape[1], 3))), axis=1)
+pad_zs_isopentane = np.concatenate((zs_isopentane, np.zeros((zs_isopentane.shape[0], n_atoms_squal - zs_isopentane.shape[1]), dtype=np.int32)), axis=1)
+
+# Methane
+idx_methane = np.asarray(data_methane.get('traj_idx'), dtype=int)
+
+idx_methane_traj = np.where(idx_methane == 14)[0]
+
+xyz_methane = np.array(data_methane.get("xyz"))[idx_methane_traj]
+zs_methane = np.array(data_methane.get("zs"), dtype=np.int32)[idx_methane_traj]
+ene_methane = np.array(data_methane.get("ene"))[idx_methane_traj]*2625.5 - ref_ene
+fn_methane = np.asarray(data_methane.get('Filenumber'), dtype=int)[idx_methane_traj]
+idx_methane = idx_methane[idx_methane_traj]
+
+idx_methane, fn_methane, ene_methane, xyz_methane, zs_methane = sort(idx_methane, fn_methane, ene_methane, xyz_methane, zs_methane)
+
+pad_xyz_methane = np.concatenate((xyz_methane, np.zeros((xyz_methane.shape[0], n_atoms_squal - xyz_methane.shape[1], 3))), axis=1)
+pad_zs_methane = np.concatenate((zs_methane, np.zeros((zs_methane.shape[0], n_atoms_squal - zs_methane.shape[1]), dtype=np.int32)), axis=1)
+
+concat_xyz = np.concatenate((pad_xyz_methane, pad_xyz_isopentane, xyz_squal))
+concat_ene = np.concatenate((ene_methane, ene_isopentane, ene_squal))
+concat_zs = np.concatenate((pad_zs_methane, pad_zs_isopentane, zs_squal))
+
+zs_for_scaler = list(zs_methane) + list(zs_isopentane) + list(zs_squal)
+
+scaling = pickle.load(open("../../scaler/scaler.pickle", "rb"))
+concat_ene_scaled = scaling.transform(zs_for_scaler, concat_ene)
+
+
+n_basis = 16
+r_min = 0.8
+r_cut = 3.24756528424191
+tau = 1.3399321961520696
+eta = 4 * np.log(tau) * ((n_basis-1)/(r_cut - r_min))**2
+zeta = - np.log(tau) / (2*np.log(np.cos(np.pi/(4*n_basis-4))))
+
+acsf_params={"nRs2":n_basis, "nRs3":n_basis, "nTs":n_basis, "rcut":r_cut, "acut":r_cut, "zeta":zeta, "eta":eta}
+
+# Generate estimator
+estimator = ARMP(iterations=1130, l1_reg=9.6983542971344e-06, l2_reg=2.7847233160230936e-06, learning_rate=0.0009050324762982482, representation_name='acsf', representation_params=acsf_params, tensorboard=True, store_frequency=10, hidden_layer_sizes=(371,94,), batch_size=73)
+
+
+estimator.load_nn("saved_model")
+
+estimator.set_properties(concat_ene_scaled)
+estimator.generate_representation(concat_xyz, concat_zs, method='fortran')
+
+pred_idx_methane = list(range(len(ene_methane)))
+pred_methane = estimator.predict(pred_idx_methane)
+print("Methane trajectory score: %s" % str(estimator.score(pred_idx_methane)))
+
+pred_idx_isopentane = list(range(len(ene_methane), len(ene_methane)+len(ene_isopentane)))
+pred_isopentane = estimator.predict(pred_idx_isopentane)
+print("Isopentane trajectory score: %s" % str(estimator.score(pred_idx_isopentane)))
+
+
+pred_idx_squal = list(range(len(ene_methane)+len(ene_isopentane), len(ene_methane)+len(ene_isopentane)+len(ene_squal)))
+pred_squal = estimator.predict(pred_idx_squal)
+print("Squalane trajectory score: %s" % str(estimator.score(pred_idx_squal)))
+
+np.savez("sorted_predictions_right_model.npz", pred_methane, concat_ene_scaled[pred_idx_methane], pred_isopentane, concat_ene_scaled[pred_idx_isopentane], pred_squal, concat_ene_scaled[pred_idx_squal])
